2a64edfa1513a194f97e9b9151ae13fc03dab8c5
Emily Perong <perongemily@gmail.com>
2026-04-26 02:22:06 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 const firebaseConfig = {
@@ -8,10 +8,9 @@ const firebaseConfig = {
   projectId: "promptvaultusa",
   storageBucket: "promptvaultusa.firebasestorage.app",
   messagingSenderId: "960105895017",
-  appId: "1:960105895017:web:1aa79742d36960d2bfbef8"
+  appId: "1:960105895017:web:1aa79742d36960d2bfbef8",
 };
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const googleProvider = new GoogleAuthProvider();
